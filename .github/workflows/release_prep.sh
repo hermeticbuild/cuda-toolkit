f69e2c6ebd77219fdb34e1c0ebae4e26d0de85e9
Corentin Kerisit <corentin.kerisit@gmail.com>
2026-03-08 23:13:20 +0000

    Add BCR github workflows

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail -x
+
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
+TAG=$1
+VERSION=${TAG:1}
+# The prefix is chosen to match what GitHub generates for source archives
+# This guarantees that users can easily switch from a released artifact to a source archive
+# with minimal differences in their code (e.g. strip_prefix remains the same)
+PREFIX="cuda_toolkit-$VERSION"
+ARCHIVE="cuda_toolkit-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+
+# SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
+# docs="$(mktemp -d)"; targets="$(mktemp)"
+# bash "$SCRIPT_DIR/build_public_starlark_docs.sh" "$targets" "$docs"
+# tar --create --auto-compress \
+#     --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+#     --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
+
+cat << EOF
+## Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "cuda_toolkit", version = "$VERSION")
+
+cuda_ext = use_extension("//extensions:cuda.bzl", "cuda")
+cuda_ext.configure(
+    cuda_version = "13.1.1",
+    cudnn_version = "9.8.0",
+    # Optional:
+    # cuda_umd_version = "13.0.0",
+)
+use_repo(cuda_ext, "cuda")
+\`\`\`
+EOF
