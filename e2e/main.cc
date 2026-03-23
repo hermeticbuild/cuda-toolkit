3d96083bf14c5a5623fd8ab01414b0ed58ace36f
Corentin Kerisit <corentin.kerisit@gmail.com>
2026-03-23 12:48:11 +0100

    use if_cuda_version for smoke tests

--- e2e/main.cc
+++ e2e/main.cc
@@ -3,16 +3,40 @@
 #include <nvrtc.h>
 #include <nvtx3/nvToolsExt.h>
 
-#if defined(CUDA_SMOKE_HAVE_CUDA_CRT)
+#if defined(CUDA_SMOKE_HAVE_CUBLAS)
 #include <cublasLt.h>
 #include <cublas_v2.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_DRIVER_API)
 #include <cuda.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_PROFILER_API)
 #include <cuda_profiler_api.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUDART)
 #include <cuda_runtime_api.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUFFT)
 #include <cufft.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUPTI)
 #include <cupti.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CURAND)
 #include <curand.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUSOLVER)
 #include <cusolverDn.h>
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUSPARSE)
 #include <cusparse.h>
 #endif
 
@@ -47,18 +71,45 @@ int main() {
   nvtxRangeId_t range_id = 0;
   Use(range_id);
 
-#if defined(CUDA_SMOKE_HAVE_CUDA_CRT)
+#if defined(CUDA_SMOKE_HAVE_DRIVER_API)
   Use(&cuDriverGetVersion);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUDART)
   Use(&cudaGetDeviceCount);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_PROFILER_API)
   Use(&cudaProfilerStart);
   Use(&cudaProfilerStop);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUBLAS)
   Use(&cublasCreate);
   Use(&cublasLtCreate);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUFFT)
   Use(&cufftPlan1d);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUPTI)
   Use(&cuptiGetVersion);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CURAND)
   Use(&curandCreateGenerator);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUSOLVER)
   Use(&cusolverDnCreate);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUSPARSE)
   Use(&cusparseCreate);
+#endif
+
+#if defined(CUDA_SMOKE_HAVE_CUDA_CRT)
   Use(&CudaCrtSmoke);
 #endif
 
